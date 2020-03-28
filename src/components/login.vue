84a5d9167f0d2e9f31648804fe7a515760e37b3c
HouseWei <1151685042@qq.com>
2020-03-28 22:17:18 +0800

    2020/3/28 22:17(sleep)

--- src/components/login.vue
+++ src/components/login.vue
@@ -43,12 +43,12 @@ export default {
         // 验证用户名
         username: [
           { required: true, message: '请输入用户名', trigger: 'blur' },
-          { min: 2, max: 10, message: '长度在 2 到 10 个字符', trigger: 'blur' }
+          { min: 3, max: 10, message: '长度在 2 到 10 个字符', trigger: 'blur' }
         ],
         // 验证密码
         password: [
           { required: true, message: '请输入密码', trigger: 'blur' },
-          { min: 3, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
         ]
       }
     }
